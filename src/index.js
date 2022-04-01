6c72117d4d5ccc2218615340b60601e1c4925a03
Daniel Baker <danpbaker90@gmail.com>
2022-04-01 11:18:51 +0100

    Update implementation after imburse-checkout changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { applyPolyfills, defineCustomElements } from '@imburseag/imburse-checkout/loader';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
